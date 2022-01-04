3ab045cc561b4a9139591a9f4f8760435714357a
David Ainslie <dainslie@gmail.com>
2022-01-04 22:18:36 +0000

    Can't decide if I should remove the generic Sttp interpreter in favour of the Monad specific one using IO

--- src/it/scala/com/backwards/algebra/interpreter/CoproductIOStreamInterpreterApp.scala
+++ src/it/scala/com/backwards/algebra/interpreter/CoproductIOStreamInterpreterApp.scala
@@ -1,7 +1,8 @@
 package com.backwards.algebra.interpreter
 
+import scala.util.Try
 import cats.InjectK
-import cats.data.EitherK
+import cats.data.{EitherK, EitherT}
 import cats.effect.{IO, IOApp}
 import cats.free.Free
 import cats.implicits._
@@ -102,9 +103,8 @@ object CoproductIOStreamInterpreterApp extends IOApp.Simple with WithAwsContaine
   // TODO A RetryingBackend (and maybe Rate Limit): https://sttp.softwaremill.com/en/latest/backends/wrappers/custom.html
   def run: IO[Unit] =
     AsyncHttpClientCatsBackend[IO]().flatMap(backend =>
-      program
-        .foldMap(SttpInterpreter(backend.logging) or S3IOInterpreter(s3Client))
-        .map(response => scribe.info(new String(response.readAllBytes)))
-        >> backend.close()
+      EitherT(program.foldMap(SttpInterpreter(backend.logging) or S3IOInterpreter(s3Client)).attempt)
+        .subflatMap(response => Try(new String(response.readAllBytes)).toEither)
+        .fold(scribe.error(_), scribe.info(_)) >> backend.close()
     )
 }
---
+++ src/main/scala/com/backwards/http/interpreter/SttpIOInterpreter.scala
@@ -0,0 +1,74 @@
+package com.backwards.http.interpreter
+
+import cats.effect.IO
+import cats.implicits._
+import cats.~>
+import eu.timepit.refined.auto._
+import sttp.client3._
+import sttp.model.Uri
+import com.backwards.auth.{Credentials, Password, User}
+import com.backwards.fp.FunctionOps.syntax._
+import com.backwards.http.Http._
+import com.backwards.http._
+
+// TODO - Factor out repetition
+object SttpIOInterpreter {
+  def apply(backend: SttpBackend[IO, Any]): Http ~> IO =
+    new (Http ~> IO) {
+      override def apply[A](fa: Http[A]): IO[A] =
+        fa match {
+          case Post(uri, headers, params, auth, body, serialiser, deserialiser) =>
+            basicRequest
+              .post(Uri(uri).addParams(params.value))
+              .optional(serialiser.contentType)(_.contentType)
+              .headers(headers.value)
+              .optional(auth)(applyAuth)
+              .optional(body)(request => body => request.body(serialiser.serialise(body)))
+              .response(asByteArray.mapRight(deserialiser.deserialise))
+              .send(backend)
+              .flatMap(response =>
+                response.body.fold(
+                  errorMsg => IO.raiseError(HttpError(errorMsg, response.code)),
+                  _.fold(IO.raiseError, IO.pure)
+                )
+              )
+
+          case Put(uri, headers, params, auth, body, serialiser, deserialiser) =>
+            basicRequest
+              .put(Uri(uri).addParams(params.value))
+              .optional(serialiser.contentType)(_.contentType)
+              .headers(headers.value)
+              .optional(auth)(applyAuth)
+              .optional(body)(request => body => request.body(serialiser.serialise(body)))
+              .response(asByteArray.mapRight(deserialiser.deserialise))
+              .send(backend)
+              .flatMap(response =>
+                response.body.fold(
+                  errorMsg => IO.raiseError(HttpError(errorMsg, response.code)),
+                  _.fold(IO.raiseError, IO.pure)
+                )
+              )
+
+          case Get(uri, headers, params, auth, deserialiser) =>
+            basicRequest
+              .get(Uri(uri).addParams(params.value))
+              .headers(headers.value)
+              .optional(auth)(applyAuth)
+              .response(asByteArray.mapRight(deserialiser.deserialise))
+              .send(backend)
+              .flatMap(response =>
+                response.body.fold(
+                  errorMsg => IO.raiseError(HttpError(errorMsg, response.code)),
+                  _.fold(IO.raiseError, IO.pure)
+                )
+              )
+        }
+    }
+
+  def applyAuth(request: RequestT[Identity, Either[String, String], Any]): Auth => RequestT[Identity, Either[String, String], Any] = {
+    case Basic(Credentials(User(user), Password(password))) => request.auth.basic(user, password)
+    case BasicToken(token) => request.auth.basicToken(token)
+    case Digest(Credentials(User(user), Password(password))) => request.auth.digest(user, password)
+    case Bearer(token, _) => request.auth.bearer(token)
+  }
+}
