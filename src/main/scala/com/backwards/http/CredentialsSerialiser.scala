6cfdafa2cc3090e1cc2d66cfc37da6d223ccd21e
David Ainslie <dainslie@gmail.com>
2022-01-08 16:16:21 +0000

    Dependent project receiving odd compilation error stating "encode" expecting String encoding but received Charset

--- src/main/scala/com/backwards/http/CredentialsSerialiser.scala
+++ src/main/scala/com/backwards/http/CredentialsSerialiser.scala
@@ -1,14 +1,13 @@
 package com.backwards.http
 
 import java.net.URLEncoder.encode
-import java.nio.charset.StandardCharsets.UTF_8
 import cats.implicits.catsSyntaxOptionId
 import com.backwards.auth.Credentials
 
 object CredentialsSerialiser { self =>
   val serialise: List[(String, String)] => Array[Byte] =
     _.map { case (key, value) =>
-      encode(key, UTF_8) + "=" + encode(value, UTF_8)
+      s"${encode(key, "UTF-8")}=${encode(value, "UTF-8")}"
     }.mkString("&").getBytes
 
   implicit object serialiserCredentialsByPassword extends Serialiser[Credentials] {
