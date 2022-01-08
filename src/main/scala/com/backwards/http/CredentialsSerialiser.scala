7d2b95ac0c0558f2d9a7954374d7c2fe8e89fb93
David Ainslie <dainslie@gmail.com>
2022-01-08 16:22:33 +0000

    Remove more code repetition

--- src/main/scala/com/backwards/http/CredentialsSerialiser.scala
+++ src/main/scala/com/backwards/http/CredentialsSerialiser.scala
@@ -4,18 +4,20 @@ import java.net.URLEncoder.encode
 import cats.implicits.catsSyntaxOptionId
 import com.backwards.auth.Credentials
 
-object CredentialsSerialiser { self =>
-  val serialise: List[(String, String)] => Array[Byte] =
-    _.map { case (key, value) =>
-      s"${encode(key, "UTF-8")}=${encode(value, "UTF-8")}"
-    }.mkString("&").getBytes
-
-  implicit object serialiserCredentialsByPassword extends Serialiser[Credentials] {
+object CredentialsSerialiser {
+  sealed trait CredentialsSerialiser extends Serialiser[Credentials] {
     val contentType: Option[String] =
       "application/x-www-form-urlencoded".some
 
+    val serialise: List[(String, String)] => Array[Byte] =
+      _.map { case (key, value) =>
+        s"${encode(key, "UTF-8")}=${encode(value, "UTF-8")}"
+      }.mkString("&").getBytes
+  }
+
+  implicit object serialiserCredentialsByPassword extends CredentialsSerialiser {
     def serialise(credentials: Credentials): Array[Byte] =
-      self.serialise(
+      serialise(
         List(
           "grant_type" -> "password",
           "username" -> credentials.user.value.value,
@@ -24,12 +26,9 @@ object CredentialsSerialiser { self =>
       )
   }
 
-  implicit object serialiserCredentialsByClientCredentials extends Serialiser[Credentials] {
-    val contentType: Option[String] =
-      "application/x-www-form-urlencoded".some
-
+  implicit object serialiserCredentialsByClientCredentials extends CredentialsSerialiser {
     def serialise(credentials: Credentials): Array[Byte] =
-      self.serialise(
+      serialise(
         List(
           "grant_type" -> "client_credentials",
           "client_id" -> credentials.user.value.value,
