8ddd3d9030a2233031e3254c436c090a08ab3a18
David Ainslie <dainslie@gmail.com>
2022-01-16 16:35:01 +0000

    Monoid for Jsonl

--- src/main/scala/com/backwards/json/Jsonl.scala
+++ src/main/scala/com/backwards/json/Jsonl.scala
@@ -1,11 +1,20 @@
 package com.backwards.json
 
-import cats.Show
+import cats.implicits._
+import cats.{Monoid, Show}
 import io.circe.Json
 
-final case class Jsonl(value: Vector[Json]) extends AnyVal
+final case class Jsonl(value: Vector[Json] = Vector.empty) extends AnyVal
 
 object Jsonl {
+  implicit val monoidJsonl: Monoid[Jsonl] = new Monoid[Jsonl] {
+    def empty: Jsonl =
+      Jsonl()
+
+    def combine(x: Jsonl, y: Jsonl): Jsonl =
+      Jsonl(x.value combine y.value)
+  }
+
   implicit val showJsonl: Show[Jsonl] =
     _.value.map(_.noSpaces).mkString("\n")
 
